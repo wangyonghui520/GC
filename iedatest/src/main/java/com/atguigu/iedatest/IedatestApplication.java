badd84183157e04eb8c8e5e87e65725ae56fd590
王永辉 <snow20@aliyun.com>
2019-11-18 17:34:18 +0800

    Initial commit

---
+++ iedatest/src/main/java/com/atguigu/iedatest/IedatestApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.iedatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IedatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IedatestApplication.class, args);
+	}
+
+}
---
+++ iedatest/src/test/java/com/atguigu/iedatest/IedatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.iedatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IedatestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
